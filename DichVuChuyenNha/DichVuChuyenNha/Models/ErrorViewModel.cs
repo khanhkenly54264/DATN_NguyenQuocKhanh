0168662c52088415eb3907425be063584cbbca5c
khanhkenly54264 <khanhkenly70@gmail.com>
2025-05-10 01:12:32 +0700

    update 6

--- DichVuChuyenNha/DichVuChuyenNha/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DichVuChuyenNha.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
